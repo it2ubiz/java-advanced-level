d25b0c682d3533aeafbbf13a658353dfa3adb079
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2020-11-26 00:41:48 +0400

    Added solution

--- src/main/java/org/stepik/lesson2/QuestionAnswer_1_2_3.java
+++ src/main/java/org/stepik/lesson2/QuestionAnswer_1_2_3.java
@@ -1,4 +1,14 @@
 package org.stepik.lesson2;
 
+/**
+ * @author Zurbaevi Nika
+ */
 public class QuestionAnswer_1_2_3 {
+    /*
+     * Вопрос:
+     * Select a correct statement about methods and overloading.
+     *
+     * Ответ:
+     * Two methods can have the same name, the same returning type but different type of arguments.
+     * */
 }
