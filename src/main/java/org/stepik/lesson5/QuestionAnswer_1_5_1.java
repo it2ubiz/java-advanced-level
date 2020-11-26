5eaee2adaa87892afb0f10bbad5e7d5a544caf96
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2020-11-26 15:08:10 +0400

    Added solution

---
+++ src/main/java/org/stepik/lesson5/QuestionAnswer_1_5_1.java
@@ -0,0 +1,16 @@
+package org.stepik.lesson5;
+
+/**
+ * @author Zurbaevi Nika
+ */
+public class QuestionAnswer_1_5_1 {
+    /*
+     * Вопрос:
+     * An enum can include ...
+     *
+     * Ответ:
+     * public method
+     * private constructor
+     * predefined constants
+     * */
+}
---
+++ src/main/java/org/stepik/lesson5/QuestionAnswer_1_5_2.java
@@ -0,0 +1,31 @@
+package org.stepik.lesson5;
+
+/**
+ * @author Zurbaevi Nika
+ */
+public class QuestionAnswer_1_5_2 {
+    /*
+     * Вопрос:
+     * Declare an enum Currency.
+     *
+     * It should include the following currency codes (according to ISO 4217):
+     *
+     * USD - United States dollar
+     * EUR - Euro
+     * GBP - Pound sterling
+     * RUB - Russian ruble
+     * UAH - Ukrainian hryvnia
+     * KZT - Kazakhstani tenge
+     * CAD - Canadian dollar
+     * JPY - Japanese yen
+     * CNY - Chinese yuan
+     * You must include all of the above codes, others do not. The constant in the enum can be declared in any order.
+     *
+     * Ответ: см.ниже
+     * */
+
+    public enum Currency {
+        USD, EUR, GBP, RUB, UAH, KZT, CAD, JPY, CNY
+    }
+
+}
