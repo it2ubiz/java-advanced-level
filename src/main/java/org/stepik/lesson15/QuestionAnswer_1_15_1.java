09c53fa736a24fa26e29bbbb93793f3530dc026e
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2020-11-29 00:21:40 +0400

    Added solution

---
+++ src/main/java/org/stepik/lesson15/QuestionAnswer_1_15_1.java
@@ -0,0 +1,16 @@
+package org.stepik.lesson15;
+
+/**
+ * @author Zurbaevi Nika
+ */
+public class QuestionAnswer_1_15_1 {
+    /*
+     * Вопрос:
+     *
+     * Select existing iterators.
+     *
+     * Ответ:
+     * ListIterator
+     * Iterator
+     * */
+}
