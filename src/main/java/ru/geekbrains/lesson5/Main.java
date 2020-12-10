771417bef9217326f7781dd09c68c6caf2399f37
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2020-12-10 14:09:09 +0400

    Fixed minor lack

--- src/main/java/ru/geekbrains/lesson5/Main.java
+++ src/main/java/ru/geekbrains/lesson5/Main.java
@@ -4,8 +4,8 @@ package ru.geekbrains.lesson5;
  * @author Zurbaevi Nika
  */
 public class Main {
-    static final int size = 10000000;
-    static final int h = size / 2;
+    static final int SIZE = 10000000;
+    static final int H = SIZE / 2;
 
     public static void main(String[] args) {
         System.out.println("Execution time of the 1st method: " + firstMethod());
@@ -13,36 +13,36 @@ public class Main {
     }
 
     private static long firstMethod() {
-        float[] array = new float[size];
-        for (int i = 0; i < size; i++) {
+        float[] array = new float[SIZE];
+        for (int i = 0; i < SIZE; i++) {
             array[i] = 1;
         }
         long a = System.currentTimeMillis();
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < SIZE; i++) {
             array[i] = (float) (array[i] * Math.sin(0.2f + (float) i / 5) * Math.cos(0.2f + (float) i / 5) * Math.cos(0.4f + (float) i / 2));
         }
         return System.currentTimeMillis() - a;
     }
 
     private static long secondMethod() {
-        float[] array = new float[size];
-        for (int i = 0; i < size; i++) {
+        float[] array = new float[SIZE];
+        for (int i = 0; i < SIZE; i++) {
             array[i] = 1;
         }
-        float[] firstArray = new float[h];
-        float[] secondArray = new float[h];
+        float[] firstArray = new float[H];
+        float[] secondArray = new float[H];
         long a = System.currentTimeMillis();
-        System.arraycopy(array, 0, firstArray, 0, h);
-        System.arraycopy(array, h, secondArray, 0, h);
+        System.arraycopy(array, 0, firstArray, 0, H);
+        System.arraycopy(array, H, secondArray, 0, H);
         Thread firstThread = new Thread(() -> {
-            for (int i = 0; i < h; i++) {
+            for (int i = 0; i < H; i++) {
                 firstArray[i] = (float) (firstArray[i] * Math.sin(0.2f + (float) i / 5) * Math.cos(0.2f + (float) i / 5) * Math.cos(0.4f + (float) i / 2));
             }
         });
         firstThread.start();
         Thread secondThread = new Thread(() -> {
-            for (int i = 0; i < h; i++) {
-                secondArray[i] = (float) (secondArray[i] * Math.sin(0.2f + (float) (i + h) / 5) * Math.cos(0.2f + (float) (i + h) / 5) * Math.cos(0.4f + (float) (i + h) / 2));
+            for (int i = 0; i < H; i++) {
+                secondArray[i] = (float) (secondArray[i] * Math.sin(0.2f + (float) (i + H) / 5) * Math.cos(0.2f + (float) (i + H) / 5) * Math.cos(0.4f + (float) (i + H) / 2));
             }
         });
         secondThread.start();
@@ -52,8 +52,8 @@ public class Main {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.arraycopy(firstArray, 0, array, 0, h);
-        System.arraycopy(secondArray, 0, array, h, h);
+        System.arraycopy(firstArray, 0, array, 0, H);
+        System.arraycopy(secondArray, 0, array, H, H);
         return System.currentTimeMillis() - a;
     }
 }
